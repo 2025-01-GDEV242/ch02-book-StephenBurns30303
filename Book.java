f6b43d7c1518e590275343ef4ad45305febd051e
StephenBurns <123398787+StephenBurns30303@users.noreply.github.com>
2025-02-03 12:08:56 -0500

    88- Added a new field call refNumber
    added the refNumber field along the changes needed to incorporate it

--- Book.java
+++ Book.java
@@ -12,6 +12,7 @@ class Book
     private String author;
     private String title;
     private int pages;
+    private String refNumber;
 
     /**
      * Set the author and title fields when this object
@@ -22,6 +23,23 @@ class Book
         author = bookAuthor;
         title = bookTitle;
         pages = bookPages;
+        refNumber="";
+    }
+    
+    /**
+     * Applies individual reference number for book
+     */
+    public void setRefNumber(String ref)
+    {
+        refNumber=ref;
+    }
+    
+    /**
+     *  return the reference number
+     */
+    public String getRefNumber()
+    {
+        return refNumber;
     }
     
     /**
